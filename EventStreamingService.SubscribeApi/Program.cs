9bb6f0ee614e8eea9a570bbb04f3e2ee2128cb31
bogdaner2 <KonorinDeveloper@gmail.com>
2020-05-01 16:21:18 +0300

    Added api for subscribers

---
+++ EventStreamingService.SubscribeApi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace EventStreamingService.SubscribeApi
+{
+    class Program
+    {
+        private const string HostUrl = "http://localhost:1234";
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(new Uri(HostUrl)))
+            {
+                host.Start();
+                
+                Console.WriteLine("Host started on " + HostUrl);
+                Console.ReadKey();
+            }
+        }
+    }
+
+    public class RoutingModule : NancyModule
+    {
+        public RoutingModule()
+        {
+            Get("/ping", _ => "Okay");
+
+            Get("/", async (args, ct) => await Task.FromResult(200));
+        }
+    }
+}
