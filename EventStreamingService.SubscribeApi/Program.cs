868111f36c35adaf7b4b4fd9a86d3f1cad175373
bogdaner2 <KonorinDeveloper@gmail.com>
2020-05-02 19:20:39 +0300

    Removed Nancy

--- EventStreamingService.SubscribeApi/Program.cs
+++ EventStreamingService.SubscribeApi/Program.cs
@@ -1,32 +1,26 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using Nancy;
-using Nancy.Hosting.Self;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace EventStreamingService.SubscribeApi
 {
-    class Program
+    public class Program
     {
-        private const string HostUrl = "http://localhost:1234";
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            using (var host = new NancyHost(new Uri(HostUrl)))
-            {
-                host.Start();
-                
-                Console.WriteLine("Host started on " + HostUrl);
-                Console.ReadKey();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
-    }
 
-    public class RoutingModule : NancyModule
-    {
-        public RoutingModule()
-        {
-            Get("/ping", _ => "Okay");
-
-            Get("/", async (args, ct) => await Task.FromResult(200));
-        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
